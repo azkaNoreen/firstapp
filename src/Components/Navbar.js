6dc3f128485c66ef80a6f497bdeef880c83e4598
Azka Noreen <azka.noreen6@gmail.com>
2022-11-09 12:53:34 +0500

    Navbar Added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar(props) {
+  return (
+    <>
+    <nav>
+    <div class="topnav">
+  <a class="active" href="#home">VoteToken</a>
+  <a href="/">Add Coin</a>
+  <a href="/">Vote Coin</a>
+  <a href="/">Search Coin</a>
+  <div class="topnav-right">
+    <a href="/">Log In</a>
+    <a href="/">Sign Up</a>
+  </div>
+</div>
+</nav>
+   
+</>
+  )
+}
